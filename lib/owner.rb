8c1ad712d5c8291df55b248b7fc31d59aef07ace
William West <wwest466@gmail.com>
2020-09-14 01:13:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,10 @@
 class Owner
   attr_reader :name, :species
+  @@all = []
   def initialize(name, species = "human")
     @name = name
     @species = species
+    @@all << self
   end
   def say_species
     p "I am a #{@species}."
